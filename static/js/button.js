aca619a3fa2c4ddfe517a8f92f61021cd926f14e
seo92js <seo92js@gmail.com>
2022-01-27 18:20:05 +0900

    bootstrap 실습중

--- static/js/button.js
+++ static/js/button.js
@@ -1,23 +1,15 @@
 function darkLightHandler(self){
-    // var target = document.querySelector('body');
-   
-    // if(self.value === 'Dark Mode'){
-    //     target.style.backgroundColor = 'black';
-    //     target.style.color = 'white';
-    //     self.value = 'Light Mode';
-    // }else{
-    //     target.style.backgroundColor = 'white';
-    //     target.style.color = 'black';
-    //     self.value = 'Dark Mode';
-    // }
-    
     if($(self).val() === 'Dark Mode'){
-        $('body').css('color', 'white');
-        $('body').css('backgroundColor', 'black');
         $(self).val('Light Mode');
+
+        $('body header div').removeClass('text-black').addClass('text-white');
+        $('.container-fluid').removeClass('text-black').addClass('text-white');
+        $('.container-fluid').removeClass('bg-light').addClass('bg-dark');
     }else{
-        $('body').css('color', 'black');
-        $('body').css('backgroundColor', 'white');
         $(self).val('Dark Mode');
+
+        $('body header div').removeClass('text-white').addClass('text-black');
+        $('.container-fluid').removeClass('text-white').addClass('text-black');
+        $('.container-fluid').removeClass('bg-dark').addClass('bg-light');
     }
 }
