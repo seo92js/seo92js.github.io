bc42d00bea08d31aa23eb8e00a255fce9ac3767a
seo92js <seo92js@gmail.com>
2022-01-28 12:44:32 +0900

    bootstrap 삭제

--- static/js/button.js
+++ static/js/button.js
@@ -2,14 +2,20 @@ function darkLightHandler(self){
     if($(self).val() === 'Dark Mode'){
         $(self).val('Light Mode');
 
-        $('body header div').removeClass('text-black').addClass('text-white');
-        $('.container-fluid').removeClass('text-black').addClass('text-white');
-        $('.container-fluid').removeClass('bg-light').addClass('bg-dark');
+        // $('body header div').removeClass('text-black').addClass('text-white');
+        // $('.container-fluid').removeClass('text-black').addClass('text-white');
+        // $('.container-fluid').removeClass('bg-light').addClass('bg-dark');
+
+        $('body').css('color', 'white');
+        $('body').css('backgroundColor','black');
     }else{
         $(self).val('Dark Mode');
 
-        $('body header div').removeClass('text-white').addClass('text-black');
-        $('.container-fluid').removeClass('text-white').addClass('text-black');
-        $('.container-fluid').removeClass('bg-dark').addClass('bg-light');
+        // $('body header div').removeClass('text-white').addClass('text-black');
+        // $('.container-fluid').removeClass('text-white').addClass('text-black');
+        // $('.container-fluid').removeClass('bg-dark').addClass('bg-light');
+
+        $('body').css('color', 'black');
+        $('body').css('backgroundColor', 'white');
     }
 }
